9d55be03bad718ecb415b75216e55eb3c421501e
blackbone23 <rully.lukman@gmail.com>
2024-01-29 13:49:39 +0700

    feat(redirect): remove unnecessary code from middleware, add custom 404 page and route to it

--- middleware.ts
+++ middleware.ts
@@ -21,8 +21,3 @@ export async function middleware(request: NextRequest) {
     }
   }
 }
- 
-// // See "Matching Paths" below to learn more
-// export const config = {
-//   matcher: '/dashboard/:path*',
-// }
